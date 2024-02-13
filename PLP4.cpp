8682eaa4325a85acc49ac696f27684442be3287f
dldusenberry <98852855+dldusenberry@users.noreply.github.com>
2024-02-13 15:36:14 -0800

    Update PLP4.cpp

--- PLP4.cpp
+++ PLP4.cpp
@@ -14,9 +14,10 @@ int multiplier(int x, int y)
 
 int recursiveFact(int w)
 {
-	if (w > 1)
-		w = w * recursiveFact(w - 1);
-	return w;
+	if (w ==0 || w == 1)
+		return 1;
+	return w * recursiveFact(w - 1);
+	
 }
 
 // I could not quite figure out how to  get a function to return to strings
@@ -65,31 +66,39 @@ Person splitString(const Person& names, std::string str, std::string del) {
 int main(){	
 	// while loop
 	int count = 0;
+	std::cout << "while loop: ";
 	while (count < 10) {
 		std::cout << count;
+		++count;
 	}
+	std::cout << "\n";
 
 	//do while loop
 	int j = 5;
+	std::cout << "do-while loop: ";
 	do {
-		std::cout << j--;
+		std::cout << j;
+		--j;
 	} while (j > 0);
+	std::cout << "\n";
 
 
 	//for loop (iter)
+	std::cout << "for loop: "; 
 	for (int i = 0; i < 10; ++i) {
 		std::cout << i;
 	}
+	std::cout << "\n";
 
 	// Function calls
 	int x = 4;
 	int y = 5;
 	int z = multiplier(x, y);
-	std::cout << "Multiplier = " << z;
+	std::cout << "Multiplier = " << z << "\n";
 
 	int w = 5;
 	int fact = recursiveFact(w);
-	std::cout << "Factorial = " << w;
+	std::cout << "Factorial = "<< w << "\n";
 
 	// Tried to split a string for hours with no success	:(
 
